a34c6098cc55035b0723c5011082ac1a7ef310c9
Bertrand Le Roy <bertrandleroy@gmail.com>
2021-02-16 00:31:50 -0800

    Finish and refactor the utils tests

--- tests/FluentPathTest/AsyncEnumerableUtilsTests.cs
+++ tests/FluentPathTest/AsyncEnumerableUtilsTests.cs
@@ -17,80 +17,67 @@ namespace FluentPathTest
         [Fact]
         public async Task CanEnumerateSynchronousEnumerableAsynchronously()
         {
-            var result = new List<string>();
-            await foreach (string s in TestEnumerable.ToAsyncEnumerable())
-            {
-                result.Add(s);
-            }
-            Assert.Equal(TestEnumerable, result);
+            Assert.Equal(TestEnumerable, await LogEnumeration(TestEnumerable.ToAsyncEnumerable()));
         }
 
         [Fact]
         public async Task SynchronousEnumerableMadeAsynchronousCanBeCancelled()
         {
-            var result = new List<string>();
             var source = new CancellationTokenSource();
+            (var loggingEmumerable, var log) = GetAsyncEnumerableAndLog(TestEnumerable.ToAsyncEnumerable(source.Token));
             await Assert.ThrowsAsync<TaskCanceledException>(async () =>
             {
-                await foreach (string s in TestEnumerable.ToAsyncEnumerable(source.Token))
+                await foreach (string s in loggingEmumerable)
                 {
-                    result.Add(s);
                     source.Cancel();
                 }
             });
-            Assert.Equal(new[] { "one" }, result);
+            Assert.Equal(new[] { "one" }, log);
         }
 
         [Fact]
         public async Task SelectMapsItems()
         {
-            var result = new List<string>();
-            await foreach (string s in TestAsyncEnumerable().Select(s => s + "-a-reno"))
-            {
-                result.Add(s);
-            }
-            Assert.Equal(new[] { "one-a-reno", "two-a-reno" }, result);
+            Assert.Equal(
+                new[] { "one-a-reno", "two-a-reno" },
+                await LogEnumeration(
+                    TestAsyncEnumerable()
+                        .Select(s => s + "-a-reno")));
         }
 
         [Fact]
         public async Task SelectAsynchronouslyMapsItems()
         {
-            var result = new List<string>();
             var stopwatch = new Stopwatch();
             stopwatch.Start();
-            await foreach (string s in TestAsyncEnumerable().Select(async s =>
+            var log = await LogEnumeration(TestAsyncEnumerable().Select(async s =>
             {
                 await Task.Delay(30);
                 return s + "-a-reno";
-            }))
-            {
-                result.Add(s);
-            }
+            }));
             stopwatch.Stop();
             Assert.True(stopwatch.ElapsedMilliseconds > 80);
-            Assert.Equal(new[] { "one-a-reno", "two-a-reno" }, result);
+            Assert.Equal(new[] { "one-a-reno", "two-a-reno" }, log);
         }
 
         [Fact]
         public async Task SelectNotNullSkipsNullItems()
         {
-            var result = new List<string>();
-            await foreach (string s in TestAsyncEnumerableWithNulls().SelectNotNull(s => s == "null" ? null : s + "-a-reno"))
-            {
-                result.Add(s);
-            }
-            Assert.Equal(new[] { "one-a-reno", "two-a-reno" }, result);
+            Assert.Equal(
+                new[] { "one-a-reno", "two-a-reno" },
+                await LogEnumeration(
+                    TestAsyncEnumerableWithNulls()
+                        .SelectNotNull(s => s == "null" ? null : s + "-a-reno")));
         }
 
         [Fact]
         public async Task SelectManyFlattensNestedEnumeration()
         {
-            var result = new List<int>();
-            await foreach (int i in AsyncEnumerableFrom(1..3).SelectMany(j => AsyncEnumerableFrom(1..j)))
-            {
-                result.Add(i);
-            }
-            Assert.Equal(new[] { 1, 1, 2, 1, 2, 3 }, result);
+            Assert.Equal(
+                new[] { 1, 1, 2, 1, 2, 3 },
+                await LogEnumeration(
+                    AsyncEnumerableFrom(1..3)
+                        .SelectMany(j => AsyncEnumerableFrom(1..j))));
         }
 
         [Fact]
@@ -125,27 +112,91 @@ namespace FluentPathTest
         [Fact]
         public async Task WhereFilters()
         {
-            var result = new List<int>();
-            await foreach (int i in AsyncEnumerableFrom(1..5).Where(j => j % 2 == 1))
-            {
-                result.Add(i);
-            }
-            Assert.Equal(new[] { 1, 3, 5 }, result);
+            Assert.Equal(
+                new[] { 1, 3, 5 },
+                await LogEnumeration(
+                    AsyncEnumerableFrom(1..5)
+                        .Where(j => j % 2 == 1)));
         }
 
         [Fact]
         public async Task WhereFiltersAsynchronously()
         {
-            var result = new List<int>();
-            await foreach (int i in AsyncEnumerableFrom(1..5).Where(async j =>
-            {
-                await Task.Delay(10);
-                return j % 2 == 1;
-            }))
-            {
-                result.Add(i);
-            }
-            Assert.Equal(new[] { 1, 3, 5 }, result);
+            Assert.Equal(
+                new[] { 1, 3, 5 },
+                await LogEnumeration(
+                    AsyncEnumerableFrom(1..5)
+                    .Where(async j =>
+                    {
+                        await Task.Delay(10);
+                        return j % 2 == 1;
+                    })));
+        }
+
+        [Fact]
+        public async Task AllIsTrueIfAllItemsSatisfyTheCondition()
+        {
+            Assert.True(await new[] { 1, 3, 5, 7 }.ToAsyncEnumerable().All(i => i % 2 == 1));
+        }
+
+        [Fact]
+        public async Task AllIsFalseIfAnyItemSatisfiesTheCondition()
+        {
+            Assert.False(await new[] { 1, 3, 4, 5, 7 }.ToAsyncEnumerable().All(i => i % 2 == 1));
+            Assert.False(await new[] { 1, 3, 5, 7, 8 }.ToAsyncEnumerable().All(i => i % 2 == 1));
+            Assert.False(await new[] { 0, 1, 3, 5, 7 }.ToAsyncEnumerable().All(i => i % 2 == 1));
+            Assert.False(await new[] { 0, 2 }.ToAsyncEnumerable().All(i => i % 2 == 1));
+        }
+
+        [Fact]
+        public async Task AllIsTrueOnEmptyEnumeration()
+        {
+            Assert.True(await new int[] { }.ToAsyncEnumerable().All(i => i % 2 == 1));
+        }
+
+        [Fact]
+        public async Task AnyIsFalseIfNoItemSatisfiesTheCondition()
+        {
+            Assert.False(await new[] { 1, 3, 5, 7 }.ToAsyncEnumerable().Any(i => i % 2 == 0));
+        }
+
+        [Fact]
+        public async Task AnyIsTrueIfAnyItemSatisfiesTheCondition()
+        {
+            Assert.True(await new[] { 1, 3, 4, 5, 7 }.ToAsyncEnumerable().Any(i => i % 2 == 0));
+            Assert.True(await new[] { 1, 3, 5, 7, 8 }.ToAsyncEnumerable().Any(i => i % 2 == 0));
+            Assert.True(await new[] { 0, 1, 3, 5, 7 }.ToAsyncEnumerable().Any(i => i % 2 == 0));
+            Assert.True(await new[] { 0, 2 }.ToAsyncEnumerable().Any(i => i % 2 == 0));
+        }
+
+        [Fact]
+        public async Task AnyIsFalseOnEmptyEnumeration()
+        {
+            Assert.False(await new int[] { }.ToAsyncEnumerable().Any(i => i % 2 == 0));
+        }
+
+        [Fact]
+        public async Task SkipSkips()
+        {
+            Assert.Equal(
+                new[] { 4, 5, 6 },
+                await LogEnumeration(new[] { 1, 2, 3, 4, 5, 6 }.ToAsyncEnumerable().Skip(3)));
+        }
+
+        [Fact]
+        public async Task TakeTakes()
+        {
+            Assert.Equal(
+                new[] { 1, 2, 3 },
+                await LogEnumeration(new[] { 1, 2, 3, 4, 5, 6 }.ToAsyncEnumerable().Take(3)));
+        }
+
+        [Fact]
+        public async Task EmptyIsempty()
+        {
+            Assert.Equal(
+                Array.Empty<int>(),
+                await LogEnumeration(AsyncEnumerable.Empty<int>()));
         }
 
         private static readonly string[] TestEnumerable = new[] { "one", "two" };
@@ -175,6 +226,31 @@ namespace FluentPathTest
                 yield return i;
             }
             await Task.CompletedTask;
+        }
+
+        private static async IAsyncEnumerable<T> LoggingEnumerable<T>(IAsyncEnumerable<T> enumerable, IList<T> log)
+        {
+            await foreach (T item in enumerable)
+            {
+                log.Add(item);
+                yield return item;
+            }
+        }
+
+        private static (IAsyncEnumerable<T> enumerable, IList<T> log) GetAsyncEnumerableAndLog<T>(IAsyncEnumerable<T> enumerable)
+        {
+            var log = new List<T>();
+            return (LoggingEnumerable(enumerable, log), log);
+        }
+
+        private static async ValueTask<IList<T>> LogEnumeration<T>(IAsyncEnumerable<T> enumerable)
+        {
+            var log = new List<T>();
+            await foreach(T item in enumerable)
+            {
+                log.Add(item);
+            }
+            return log;
         }
     }
 }
